a28ec913963f54b64e37b987b32ab0f0580ab2ca
Marri Kang <kangmarri@gmail.com>
2021-03-05 15:01:55 -0500

    Add function for ls

--- bash.js
+++ bash.js
@@ -1,10 +1,4 @@
-process.stdout.write("prompt > ");
-
-process.stdin.on("data", (data) => {
-  const cmd = data.toString().trim();
-
-  if (cmd === "pwd") {
-    process.stdout.write(`${process.cwd()}`);
-    process.stdout.write("\nprompt > ");
-  }
-});
+const runBash = require('./pwd.js');
+const runLS = require('./ls.js');
+runBash();
+runLS();
---
+++ ls.js
@@ -0,0 +1,19 @@
+const fs = require('fs')
+
+module.exports = function(){
+    fs.readdir('./', 'utf8', (err,files) => {
+    if (err) {
+        throw err
+    } else {
+
+        process.stdin.on("data", (data) => {
+          const cmd = data.toString().trim();
+        
+          if (cmd === "ls") {
+            process.stdout.write(files.join('\n'))
+            process.stdout.write("\nprompt > ");
+          }
+        });
+    }
+})
+}
